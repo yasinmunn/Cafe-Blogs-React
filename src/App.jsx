1435bce74d79c0d1a7c03cc913af972d345ba2f7
yasinmunn <yasinmunn@gmail.com>
2024-03-30 18:51:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header></Header>
+
+      <div className='md:flex mx-auto container'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+const Blog = ({Blog}) => {
+
+    console.log(blog);
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Blog.propTypes = {
+    blog: PropTypes.object
+}
+
+export default Blog;
