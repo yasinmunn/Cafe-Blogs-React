040da191b19bd8232bb84a913c77a6abc94d8d4c
yasinmunn <yasinmunn@gmail.com>
2024-04-09 17:36:04 +0600

    Finale-Commit

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -14,7 +14,7 @@ const Blogs = ({handleAddToBookmark, }) => {
     return (
 
         <div className="md:w-2/3">
-            <h1 className="text-4xl">Blogs: {blogs.length} </h1>
+            <h1 className="text-4xl">BLOGS: {blogs.length} </h1>
             {
                 blogs.map(blog => <Blog key={blog.id} blog={blog} handleAddToBookmark={handleAddToBookmark}>
                     
